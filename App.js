61efea1f8211a9f4a992340b9058eb685161f6cc
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-07 18:47:26 -0400

    Text

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your money!!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.js to start working on your money Money mone y moeneyh
+        eueu!!
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems:'center',
-    // justifyContent:'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "dodgerblue",
   },
 });
