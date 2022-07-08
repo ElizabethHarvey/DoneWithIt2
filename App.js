f5989ec8f25f100227485908b4beba15b1d71f61
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-08 12:29:55 -0400

    Absolute and Relative Positioning

--- App.js
+++ App.js
@@ -10,15 +10,13 @@ export default function App() {
         flexDirection: "row", // horizontal
         justifyContent: "center", // main
         alignItems: "center", // secondary
-        alignContent: "center", // Only works with wrapping
-        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
+          height: 100,
         }}
       />
       <View
@@ -26,25 +24,14 @@ export default function App() {
           backgroundColor: "gold",
           width: 100,
           height: 100,
+          top: 20,
+          left: 20,
+          position: "absolute",
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
