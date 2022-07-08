29a47bbd535e377bcb38c743e25e5cdfc0712827
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-08 12:18:55 -0400

    Flexbox- flexWrap and alignContent

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ export default function App() {
         flexDirection: "row", // horizontal
         justifyContent: "center", // main
         alignItems: "center", // secondary
+        alignContent: "center", // Only works with wrapping
+        flexWrap: "wrap",
       }}
     >
       <View
@@ -17,19 +19,32 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 300,
-          alignSelf: "flex-start",
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 200,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
