e4a7ea0c44faa0fdabce578e2d86238879f88520
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-08 19:31:58 -0400

    Platform-specific Code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./components/AppText";
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <MaterialCommunityIcons name="email" size={60} color="blue" />
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
--- components/AppText.js
+++ components/AppText.js
@@ -6,8 +6,19 @@ function AppText({ children }) {
 }
 
 const styles = StyleSheet.create({
-  text: 18,
-  fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  text: {
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
+  },
 });
 
 export default AppText;
