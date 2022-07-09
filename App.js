c8f670c2152729933c41315729357bc11dd915ee
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-08 20:42:58 -0400

    Improving the Welcome Screen

--- App.js
+++ App.js
@@ -5,15 +5,5 @@ import AppButton from "./components/AppButton";
 import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppButton title="Login" onPress={() => console.log("Tapped")} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
