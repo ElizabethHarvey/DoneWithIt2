f81d9a4ec52dcd4d1c7e04c23ad6f034e755abce
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-08 21:54:50 -0400

    Building the Listing Details Screen

--- App.js
+++ App.js
@@ -4,21 +4,8 @@ import AppButton from "./components/AppButton";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import Card from "./components/Card";
+import ListingDetailsScreen from "./screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
