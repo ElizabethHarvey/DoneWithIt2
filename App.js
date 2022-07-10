168b06296c7ba8bbe452f1ef56f551f5fbad9138
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-09 21:40:38 -0400

    Handling Selections

--- App.js
+++ App.js
@@ -16,16 +16,24 @@ import ListingsScreen from "./screens/ListingsScreen";
 import AppTextInput from "./components/AppTextInput";
 import AppPicker from "./components/AppPicker";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
+
 export default function App() {
-  const categories = [
-    { label: "Furniture", value: 1 },
-    { label: "Clothing", value: 2 },
-    { label: "Cameras", value: 3 },
-  ];
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
-      <AppPicker items={categories} icon="apps" placeholder="Category" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
       <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
