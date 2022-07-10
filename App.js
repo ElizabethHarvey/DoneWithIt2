df42371df0c0ca310236e27be063a3f3335a5901
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-10 19:56:19 -0400

    Nesting Navigators

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { Button, Image, Switch, Text, TextInput, View } from "react-native";
 import AppButton from "./components/Button";
 import * as Permissions from "expo-permissions";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { UserInterfaceIdiom } from "expo-constants";
-import { useNavigation } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import Card from "./components/Card";
@@ -25,48 +26,75 @@ import ListingEditScreen from "./screens/ListingEditScreen";
 import ImageInput from "./components/lists/ImageInput";
 import ImageInputList from "./components/ImageInputList";
 
-const Link = () => {
-  const navigation = useNavigation();
-
-  return (
-    <Button
-      title="View Tweets"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweets Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetDetails}
-      options={({ route }) => ({
-        title: route.params.id,
-      })}
-    />
-  </Stack.Navigator>
-);
-
 export default function App() {
+  const Link = () => {
+    const navigation = useNavigation();
+
+    return (
+      <Button
+        title="View Tweets"
+        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+      />
+    );
+  };
+
+  const Tweets = () => (
+    <Screen>
+      <Text>Tweets</Text>
+      <Link />
+    </Screen>
+  );
+
+  const TweetDetails = ({ route }) => (
+    <Screen>
+      <Text>Tweets Details {route.params.id}</Text>
+    </Screen>
+  );
+
+  const Stack = createStackNavigator();
+  const StackNavigator = () => (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen
+        name="TweetDetails"
+        component={TweetDetails}
+        options={({ route }) => ({ title: route.params.id })}
+      />
+    </Stack.Navigator>
+  );
+
+  const Account = () => (
+    <Screen>
+      <Text>Account</Text>
+    </Screen>
+  );
+
+  const Tab = createBottomTabNavigator();
+  const TabNavigator = () => (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeBackgroundColor: "tomato",
+        activeTintColor: "white",
+        inactiveBackgroundColor: "#eee",
+        inactiveTintColor: "black",
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={StackNavigator}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
