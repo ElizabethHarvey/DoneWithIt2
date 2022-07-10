b56468e88698f0392caae70fd7d4932163abb57d
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-10 11:10:06 -0400

    Custom Snippets

---
+++ components/Test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+function Test(props) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Test;
+
+import React from "react";
+import { StyleSheet, View } from "react-native";
