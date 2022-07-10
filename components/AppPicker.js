5a47d8d398591d9f5b20ba6616bb1675e8866ee8
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-09 20:40:35 -0400

    Modal

--- components/AppPicker.js
+++ components/AppPicker.js
@@ -1,28 +1,48 @@
-import React from "react";
-import { View, TextInput, StyleSheet, Platform } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./AppText";
+import Screen from "./Screen";
 import defaultStyles from "../config/styles";
 
 function AppPicker({ icon, placeholder, ...otherProps }) {
+  const [modalVisiable, setModalVisiable] = useState(false);
+
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons
-          name={icon}
-          size={20}
-          color={defaultStyles.colors.medium}
-          style={styles.icon}
-        />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <MaterialCommunityIcons
-        name="chevron-down"
-        size={20}
-        color={defaultStyles.colors.medium}
-      />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisiable(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisiable} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisiable(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 
