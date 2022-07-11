45cd4a40f7a026e6c0c3e5eada7afb444ef7431d
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-11 15:49:37 -0400

    Adding a Progress Bar

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Modal, StyleSheet, View } from "react-native";
+import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
 
 import AppText from "../components/Text";
+import colors from "../config/colors";
 
 function UploadScreen({ progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
+        {progress < 1 ? (
+          <Progress.Bar
+            color={colors.primary}
+            progress={progress}
+            width={200}
+          />
+        ) : (
+          <LottieView source={require("../assets/animations/done.json")} />
+        )}
         <AppText>{progress * 100}</AppText>
       </View>
     </Modal>
